c0d4b7bf2553913d9838659d2132fdba99ef1e1b
urvinb <urvibheda999@gmail.com>
2020-09-30 10:56:34 +0530

    test commit

--- Dart/lec1.dart
+++ Dart/lec1.dart
@@ -1,10 +1,11 @@
-//1. Static type and Type inference
+//1. Static type & Type inference
 
 import 'dart:io';
+
 main() {
   var fname = 'Urvi';
 
-  stdout.writeln("Is your first name : "+ fname + "?\n");
+  stdout.writeln("Is your first name : " + fname + "?\n");
   stdout.writeln("Enter your Last name");
   String lname = stdin.readLineSync();
 
@@ -13,17 +14,18 @@ main() {
 
   int length = 10;
   var breadth = 20;
-  var rect_area = length*breadth;
-  print('Area of rectangle with length $length and breadth $breadth is $rect_area \n');
+  var rect_area = length * breadth;
+  print(
+      'Area of rectangle with length $length and breadth $breadth is $rect_area \n');
 
   double base = 2.5;
   var height = 7.5;
-  var tri_area = (1/2)*base*height;
+  var tri_area = (1 / 2) * base * height;
   print("Area of traingle with base $base and height $height is $tri_area \n");
 
   dynamic radius = 2.5;
   var pi = 3.142;
-  var cir_area = (pi)*radius*radius;
+  var cir_area = (pi) * radius * radius;
   bool iscircle = true;
   var isrectangle = false;
   print("Ouestion : \nIs $cir_area area of circle with radius $radius ? \n");
@@ -47,7 +49,7 @@ It's written using three double quotes at
 start and three double quotes at the end""";
 
   print(str2);
-  
+
   var str_to_int = int.parse('10');
   assert(str_to_int == 10);
   var str_to_double = double.parse('9.86');
@@ -56,5 +58,4 @@ start and three double quotes at the end""";
   assert(int_to_str == '1.15');
   String float_to_str = 3.14159.toStringAsFixed(2);
   assert(float_to_str == '3.14');
-
 }
